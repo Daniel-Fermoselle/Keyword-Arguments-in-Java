a3e03d055e47b0ec371b48fd2c95ef6f7b53a55c
Daniel-Fermoselle <daniel.fermoselle@gmail.com>
2017-03-18 16:57:45 +0000

    Testing javassist

--- g13/src/ist/meic/pa/KeyConstructors.java
+++ g13/src/ist/meic/pa/KeyConstructors.java
@@ -1,9 +1,21 @@
 package ist.meic.pa;
+import javassist.*;
 
 public class KeyConstructors {
 
+	public void say(){
+		System.out.println("lasnfnasflkn");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		ClassPool cp = ClassPool.getDefault();
+        CtClass cc = cp.get("KeyConstructors");
+        CtMethod m = cc.getDeclaredMethod("say");
+        m.insertBefore("{ System.out.println(\"KeyConstructors.say():\"); }");
+        Class c = cc.toClass();
+        KeyConstructors h = (KeyConstructors)c.newInstance();
+        h.say();
 	}
 
 }
