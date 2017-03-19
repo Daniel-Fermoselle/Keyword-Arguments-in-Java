9699469300f98a83edcdd9ca181ecf5526aa5f77
Daniel-Fermoselle <daniel.fermoselle@gmail.com>
2017-03-19 17:26:42 +0000

    Started creating the injection code working just for int At the moment the injection code just works for int attributes and also right no w we are hardcoding the injection code in the constructor of the Test class to c heck if it will work

--- g13/src/ist/meic/pa/KeyConstructors.java
+++ g13/src/ist/meic/pa/KeyConstructors.java
@@ -1,23 +1,45 @@
 package ist.meic.pa;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 import javassist.*;
 
 public class KeyConstructors {
 
 	//TODO REMOVE THROWS
-	public static void main(String[] args) throws NotFoundException, CannotCompileException, InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException, SecurityException {
-		System.out.println("Hello World");
+	public static void main(String[] args) throws NotFoundException, 
+	CannotCompileException, InstantiationException, IllegalAccessException, ClassNotFoundException,
+	NoSuchMethodException, SecurityException, NoSuchFieldException {
 		Class<?> c = Class.forName(args[0]);
-		Constructor<?> m = c.getConstructor();
-		if(m.isAnnotationPresent(KeywordArgs.class)){
+		System.out.println("Hello World");
+		//Constructor<?> m = c.getDeclaredConstructor(Object[].class);
+		Test t = new Test();
+		System.out.println(t.toString());
+		/*if(m.isAnnotationPresent(KeywordArgs.class)){
+			ArrayList<String> fieldNames = new ArrayList<String>();
+			Field[] fields = c.getDeclaredFields();
+			for(Field field : fields){
+				fieldNames.add(field.getName());
+			}
 			KeywordArgs ka = m.getAnnotation(KeywordArgs.class);
 			String value = ka.value();
-			System.out.println(value);
-		}
-		else{
-			System.out.println("NOOB");
-		}
+			String[] comaSplit = value.split(",");
+			for(String temp : comaSplit){
+				String[] equalSplit = temp.split("=");
+				if(fieldNames.contains((String)equalSplit[0])){
+					/*Field setField = c.getDeclaredField(equalSplit[0]);
+					setField.getType();
+					System.out.println("equalSplit[0]: " + equalSplit[0] + " equalSplit[1]: " + equalSplit[1]);
+				}
+				
+			}
+			System.out.println(value);*/
+		
+		//}
+//		else{
+//			System.out.println("NOOB");
+//		}
 	} 
 }
--- g13/src/ist/meic/pa/Test.java
+++ g13/src/ist/meic/pa/Test.java
@@ -1,24 +1,54 @@
 package ist.meic.pa;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import javassist.*;
+
 public class Test {
-	private int age;
+	int width;
+	int height;
+	int margin;
+
+	@KeywordArgs("width=100,height=50,margin")
+	public Test(Object... args) {
+		Constructor<?> m;
+		try {
+			Class<?> c = this.getClass();
+			m = c.getDeclaredConstructor(Object[].class);
+			ArrayList<String> fieldNames = new ArrayList<String>();
+			Field[] fields = c.getDeclaredFields();
+			for (Field field : fields) {
+				fieldNames.add(field.getName());
+			}
+			KeywordArgs ka = m.getAnnotation(KeywordArgs.class);
+			String keyword = ka.value();
+			String[] comaSplit = keyword.split(",");
+			for (String temp : comaSplit) {
+					String[] equalSplit = temp.split("=");
+					if (fieldNames.contains((String) equalSplit[0])) {
+						if(equalSplit.length>1){
+							Field setField = c.getDeclaredField(equalSplit[0]);
+							setField.set(this, new Integer(equalSplit[1]));//TODO HOW TO GENERATE THAT MUCH POWER
 	
-	@KeywordArgs("age=10")
-	public Test(){
-		
-	}
-	
-	@Override
-	public String toString(){
-		return "Coco age: " + getAge();
+							System.out.println("equalSplit[0]: " + equalSplit[0] + " equalSplit[1]: " + equalSplit[1]);
+						}
+					}
+					else{
+						throw new RuntimeException("Unrecognize keyword: " + equalSplit[0]);
+					}
+
+			}
+		} catch (NoSuchMethodException | SecurityException | NoSuchFieldException | IllegalArgumentException
+				| IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		System.out.println(this.toString());
 	}
 
-	public int getAge() {
-		return age;
+	public String toString() {
+		return String.format("width:%s,height:%s,margin:%s", width, height, margin);
 	}
 
-	public void setAge(int age) {
-		this.age = age;
-	}
-	
 }
