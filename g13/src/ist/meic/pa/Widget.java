11ec7cabd4355f4267f60f6ad2863b55b3fc5a2b
Daniel-Fermoselle <daniel.fermoselle@gmail.com>
2017-03-22 20:23:55 +0000

    Bugs relate to inheritance, non primitive assignment, and empty keyword

--- g13/src/ist/meic/pa/Widget.java
+++ g13/src/ist/meic/pa/Widget.java
@@ -1,18 +1,15 @@
 import ist.meic.pa.KeywordArgs;
 
-
 class Widget {
-    int width;
-    int height;
-    int margin;
+	int width;
+	int height;
+	int margin;
 
-    @KeywordArgs("width=100,height=50,margin")
-    public Widget(Object... args) {
+	@KeywordArgs("width=100,height=50,margin=5")
+	public Widget(Object... args) {}
 
-    }
-
-    public String toString() {
-        return String.format("width:%s, height:%s, margin:%s",
-                width, height, margin);
-    }
+	public String toString() {
+		return String.format("width:%s,height:%s,margin:%s",
+				width, height, margin);
+	}
 }
