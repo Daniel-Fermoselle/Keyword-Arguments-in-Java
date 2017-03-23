ad06171a95bf0a1e5ba3be43e115330c63986ecb
Daniel-Fermoselle <daniel.fermoselle@gmail.com>
2017-03-23 11:41:00 +0000

    Implementing heritage

--- g13/src/ist/meic/pa/KeywordsTranslator.java
+++ g13/src/ist/meic/pa/KeywordsTranslator.java
@@ -1,7 +1,10 @@
 package ist.meic.pa;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import javassist.CannotCompileException;
 import javassist.ClassPool;
@@ -30,47 +33,39 @@ public class KeywordsTranslator implements Translator {
 
     //TODO REMOVE THROWS
     public static void keywordInjector(CtClass ctClass) throws ClassNotFoundException, CannotCompileException, NotFoundException {
-
-        CtField[] fields = ctClass.getFields();
-        HashMap<String, String> fieldVerifier = new HashMap<String, String>();
+        CtField[] fields = getAllFieldsInHierarchy(ctClass);
+        ArrayList<String> fieldVerifier = new ArrayList<String>();
         ArrayList<String> keywordFields = new ArrayList<String>();
+        HashMap<String,String> keywordAssignments;
         for (CtField field : fields) {
-            fieldVerifier.put(field.getName(), "");
+            fieldVerifier.add(field.getName());
         }
-        for (CtConstructor ctMethod : ctClass.getConstructors()) {
+        for (CtConstructor ctMethod : ctClass.getDeclaredConstructors()) {//TODO REVIEW BECAUSE OF getAllKeywordArgs
             Object[] annotations = ctMethod.getAnnotations();
             Object annotation = getCorrectAnnotation(annotations);
             if (annotation != null) {
                 KeywordArgs ka = (KeywordArgs) annotation;
-                String keyword = ka.value();
+                String keyword = getAllKeywordArgs(ctClass);
+                keywordAssignments = getMap(keyword);
                 if(keyword.equals("")){//EMPTY KEYWORD CASE
                 	String template="{}";
                 	ctMethod.setBody(template);
                 	return;
                 }
-                String[] comaSplit = keyword.split(",");
-                for (String temp : comaSplit) {
-                    String[] equalSplit = temp.split("=");
-                    if (fieldVerifier.keySet().contains((String) equalSplit[0])) {
-                        if (!keywordFields.contains(equalSplit[0])) {
-                            keywordFields.add(equalSplit[0]);
-                        } else {
-                            throw new RuntimeException("Duplicated Keyword in annotation: " + equalSplit[0]);
-                        }
-                        if (equalSplit.length > 1) {
-                            fieldVerifier.put(equalSplit[0], equalSplit[1]);
-                            //System.out.println("equalSplit[0]: " + equalSplit[0] + " equalSplit[1]: " + equalSplit[1]);
-                        }
+                for (String tempKeywordIc : keywordAssignments.keySet()) {
+                	System.out.println("keyword: " + tempKeywordIc + " : " + keywordAssignments.get(tempKeywordIc));
+                    if (fieldVerifier.contains(tempKeywordIc)) {
+                        keywordFields.add(tempKeywordIc);
                     } else {
-                        throw new RuntimeException("Unrecognize keyword: " + equalSplit[0]);
+                        throw new RuntimeException("Unrecognize keyword: " + tempKeywordIc);
                     }
 
                 }
                 //TO INJECT
                 String template = "";
-                for (String s : keywordFields) {//TODO NEED TO INITIALIZE NON PRIMITIVE TYPES
-                    if (!fieldVerifier.get(s).equals("")) {
-                        template = template + "this." + s + " = " + fieldVerifier.get(s) + ";";
+                for (String s : keywordAssignments.keySet()) {//TODO NEED TO INITIALIZE NON PRIMITIVE TYPES
+                    if (!keywordAssignments.get(s).equals("")) {
+                        template = template + "this." + s + " = " + keywordAssignments.get(s) + ";";
                     }
                 }
                 template = template +
@@ -102,7 +97,7 @@ public class KeywordsTranslator implements Translator {
 
                 for (String field : keywordFields) {
                     //System.out.println(fie ld);
-                    CtField fieldType = ctClass.getDeclaredField(field);
+                    CtField fieldType = getSpecificFields(fields,field);
                     String stringFieldType = fieldType.getType().getName();
                     template = template +
                             "	for (int i = 0; i < arguments.size(); i = i + 2) {" +
@@ -167,5 +162,73 @@ public class KeywordsTranslator implements Translator {
     	
     	return r;
     }
+    
+    public static CtField[] getAllFieldsInHierarchy(CtClass objectClass) {//TODO MAYBE DOESNT NEED TO BE INJECTED
+    	try {
+        Set<CtField> allFields = new HashSet<>();
+        CtField[] declaredFields = objectClass.getDeclaredFields();
+        //CtField[] Fields = objectClass.getFields();
+		if (objectClass.getSuperclass() != null) {
+		    CtClass superClass = objectClass.getSuperclass();
+		    CtField[] superClassMethods = getAllFieldsInHierarchy(superClass);
+		    allFields.addAll(Arrays.asList(superClassMethods));
+		}
+        allFields.addAll(Arrays.asList(declaredFields));
+        //allFields.addAll(Arrays.asList(Fields));
+        CtField[] temp = allFields.toArray(new CtField[allFields.size()]);
+        return temp;
+    	} catch (NotFoundException e) {
+    		e.printStackTrace();
+    		throw new RuntimeException(e.getMessage());
+    	}
+    }
+	
+	public static String getAllKeywordArgs(CtClass objectClass) {// TODO MAYBE DOESNT NEED TO BE INJECTED
+		try {
+			String keyword = "";
+			for (CtConstructor ctMethod : objectClass.getDeclaredConstructors()) {
+				Object[] annotations = ctMethod.getAnnotations();
+				Object annotation = getCorrectAnnotation(annotations);
+				if (annotation != null) {
+					KeywordArgs ka = (KeywordArgs) annotation;
+					keyword = ka.value();
+					if (objectClass.getSuperclass() != null) {
+						CtClass superClass = objectClass.getSuperclass();
+						keyword += "," + getAllKeywordArgs(superClass);
+					}
+				}
+			}
+			return keyword;
+		} catch (SecurityException | NotFoundException | ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e.getMessage());
+		}
+	}
 
+	public static CtField getSpecificFields(CtField[] cf, String n) {//TODO MAYBE DOESNT NEED TO BE INJECTED
+    	for(CtField f : cf){
+    		if(f.getName().equals(n)){
+    			return f;
+    		}
+    	}
+    	throw new RuntimeException("No such field named: " + n);
+    }
+	//a=10,b
+	public static HashMap<String,String> getMap(String keyword){
+		HashMap<String,String> map = new HashMap<String,String>();
+		String[] coma = keyword.split(",");
+		for(String s : coma){
+			String[] equal = s.split("=");
+			if((!map.containsKey(equal[0]) || map.get(equal[0]).equals("")) && equal.length>1){
+					map.put(equal[0], equal[1]);
+			}
+			else{
+				map.put(equal[0], "");
+			}
+		}
+		return map;
+		
+		
+	}
+	
 }
